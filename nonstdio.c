aecea79a3407912491af72f631d125f6c3995e10
Misha Dynin <dynin@google.com>
2015-08-13 17:19:56 -0700

    Populated nonstdio.c

---
+++ nonstdio.c
@@ -0,0 +1,34 @@
+#ifdef _ 
+
+                      _(A)_(B)     _(C)_(D)  _(E)
+                        _(F)     _(G) _(H) _(I)_(J)
+                        _(K)      _(L) _(M)   _(N)
+                        _(O)       _(P) _(Q)_(R)
+                        _(S)         _(T) _(U)
+                      _(V)_(W)         _(X)
+
+                  _(Y)      _(Z)_(a) _(b)_(c) _(d)_(e)
+                    _(f)      _(g)     _(h)    _(i)
+                    _(j)_(k)  _(l)       _(m)_(n) 
+                    _(o)  _(p)_(q)         _(r)  
+                    _(s)      _(t)         _(u)
+                  _(v)_(w)    _(x)       _(y)_(z)
+
+#else
+#define _(z)_##z)();typedef _##z(*
+#include <stdio.h>
+typedef int(*
+#include __FILE__
+#define __(_,z)_z _(__ y){return fputs(z,stdout),y?y(0):(_z)_0;}
+#undef _
+__)();__(_0,"")__(nl,"\n")__(_," ")
+#define _(_)__(_,#_)
+#include __FILE__
+
+int main() {
+  (L)(i)(n)(k)(e)(d)(L)(i)(s)(t)(_)(N)(Y)(C)(nl);
+
+  return 0;
+}
+
+#endif
